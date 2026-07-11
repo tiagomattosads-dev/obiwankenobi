f974952efd8c1b3b759be4347644d0b38ffc08dc
Tiago Mattos <tiagomattos.ads@gmail.com>
2026-07-11 14:25:33 -0300

    correção gitignore

---
+++ js/config.js
@@ -0,0 +1,6 @@
+// Arquivo: js/config.js (Este arquivo ficará APENAS no seu computador)
+const CONFIG = {
+    SUPABASE_URL: "https://ekbkwextgcijtazgnawg.supabase.co",
+    SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVrYmt3ZXh0Z2NpanRhemduYXdnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM2MTYzNTMsImV4cCI6MjA5OTE5MjM1M30.7zolTR3OD3FhWHPE3uPr4nhWUABCXU4vH9yM0COfMo8",
+    N8N_WEBHOOK_URL: "http://localhost:5678/webhook/obi-wan" // URL local do seu cérebro
+};
